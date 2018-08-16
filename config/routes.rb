dca247a85ff2f5187c7259077e053a6b122d8823
Joseph Surla <josephsurla@gmail.com>
2018-08-16 15:12:01 -0700

    Create session for user log in

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
