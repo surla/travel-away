6f4fd716d3aec7aae0246a284144c03daa1df1c5
Joseph Surla <josephsurla@gmail.com>
2018-08-16 14:23:44 -0700

    Redirect user to show page after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.create(users_params)
+    redirect_to @user
+  end
+
+  private
+    def users_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
